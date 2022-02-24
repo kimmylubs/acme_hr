49b2b70c2294fda885c3d0b11616f1ace0a24fd5
Kim <wukimberly1@gmail.com>
2022-02-23 20:58:18 -0500

    boops

--- server.js
+++ server.js
@@ -7,16 +7,23 @@ const path = require('path');
 const Employee = conn.define('employee', {
     name: {
         type: Sequelize.STRING,
-    },
-    employeeType: {
-        type: Sequelize.ENUM('parttime', 'fulltime', 'intern'),
-        defaultValue: 'fulltime',
     }
 });
 
 app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.delete('/api/employees/:id', async(req, res, next) => {
+    try {
+        const employee = (await Employee.findByPk(req.params.id));
+        console.log(employee);
+        await employee.destory();
+        res.sendStatus(204);
+    } catch(e){
+        next(e);
+    }
+});
 
 app.get('/api/employees', async(req, res, next) => {
     try{
@@ -25,18 +32,17 @@ app.get('/api/employees', async(req, res, next) => {
         next(e)
     }
 });
-//curl localhost:3000/api/emploese
 
-const syncAndSeed = async() => {
+
+//curl localhost:3000/api/employees
+
+const init = async() => {
     await conn.sync({ force: true });
     const [moe, lucy, ethyl, joe, larry, tim] = await Promise.all(
         ['moe', 'lucy', 'ethyl', 'joe', 'larry', 'tim']
-	    .map ( name => Employee.create({ name }))
-    );
-    ethyl.employeeType = 'parttime';
-    await (ethyl.save());
+	    .map ( name => Employee.create({ name })))    
 };
 
-syncAndSeed();
+init();
 
 app.listen(process.env.PORT || 3000);
--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ const ul = document.querySelector('ul');
 ul.addEventListener('click', async(event) => {
     if (event.target.tagName === 'LI') {
         const id = event.target.getAttribute('data-id');
-        axios.delete(`/api/employees/${id}`);
-        console.log(id);
+        await axios.delete(`/api/employees/${id}`);
+        // console.log(id)
+        // init();
+        
     }
 });
 
-const init = async() => {
-    const employees = (await axios.get('/api/employees') ).data;
+const init = async() => { 
+    const employees = (await axios.get('/api/employees')).data;
     const html = employees.map(employee => {
-        return `
-         <li data-id='${employee.id}'> ${employee.name} </li>
-        `;
-    }).join('');
+        return `<li data-id='${employee.id}'> ${employee.name} </li>`}).join('');
     ul.innerHTML = html;
 
 };
